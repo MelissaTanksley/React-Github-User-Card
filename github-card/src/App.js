3e6ae673c7c47a251518e08bc917008e2c43156b
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-09-29 13:27:32 -0700

    compiles and renders without user info

--- github-card/src/App.js
+++ github-card/src/App.js
@@ -12,40 +12,73 @@ import {
 } from "reactstrap";
 
 class App extends React.Component {
-  state = {
-    gitdata: [],
-  };
+  constructor() {
+    super();
+    this.state = {
+      gitdata: [],
+      gitusers: []
 
+    };
+  }
   componentDidMount() {
     axios
-      .get("https://api.github.com/users/melissatanksley")
+    .get("https://api.github.com/users/melissatanksley")
       .then((res) => {
-        this.setState({ gitdata: res.data });
+        this.setState([{gitdata: res.data}])
+        console.log(this.state.gitdata)
+        console.log(res.data)
       })
       .catch((error) => {
         console.log(`Everything is broken: ${error}`);
       });
-  }
-
+      axios
+        .get("https://api.github.com/users/     melissatanksley/followers")
+        .then((res) => {
+        this.setState({gitusers: res.data});
+        console.log(this.state.gitusers)
+        console.log(res.data)
+      })
+        .catch((error) => {
+        console.log(`Everything is broken: ${error}`);
+    });
+}
 
   render() {
     return (
       <>
         <h1>User Card</h1>
 
-        <Card>
-        <CardBody>
-          <CardTitle>{this.state.gitdata.name}</CardTitle>
-          <CardSubtitle>{this.state.gitdata.login}</CardSubtitle>
-        </CardBody>
-        <img width="100%" src={this.state.gitdata.avatar_url} alt={this.state.gitdata.name} />
-        <CardBody>
-          <CardText>{this.state.gitdata.bio}</CardText>
-          <CardLink href={this.state.html_url}>GitHub</CardLink>
+        {this.state.gitdata.map((user) => {
+          return (
+            <Card key={this.state.gitdata.id}>
+              <CardBody>
+              <CardTitle>{this.state.gitdata.name}</CardTitle>
+                <CardSubtitle>{this.state.gitdata.login}</CardSubtitle>
+              </CardBody>
+              <CardImg width="100%" src={this.state.gitdata.avatar_url} alt={this.state.gitdata.name} />
+              <CardBody>
+              <CardText>{this.state.gitdata.bio}</CardText>
+                <CardLink href={this.state.gitdata.html_url}>GitHub</CardLink>
+              </CardBody>
+            </Card>
+          );
+        })}
 
-        </CardBody>
-      </Card>
-
+        {this.state.gitusers.map((user) => {
+          return (
+            <Card key={user.id}>
+              <CardBody>
+                <CardTitle>{user.name}</CardTitle>
+                <CardSubtitle>{user.login}</CardSubtitle>
+              </CardBody>
+              <CardImg width="100%" src={user.avatar_url} alt={user.name} />
+              <CardBody>
+                <CardText>{user.bio}</CardText>
+                <CardLink href={user.html_url}>GitHub</CardLink>
+              </CardBody>
+            </Card>
+          );
+        })}
       </>
     );
   }
